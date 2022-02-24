2cd146a14465c11dc1f4c030a30913e3e399a9ac
Alejandro Rodriguez <alejanmrodriguez@gmail.com>
2022-02-23 23:09:39 -0500

    Completed project and added CSS

--- script.js
+++ script.js
@@ -5,10 +5,16 @@ function Book(title, author, numOfPages, readBoolean) {
     this.author = author;
     this.pages = numOfPages;
     this.read = readBoolean;
-    this.readText = readBoolean ? 'Read' : 'Not Read Yet';
+    this.readText = readBoolean ? 'You\'ve read this book!' : 'Not Read Yet';
     this.info = function() {
-        return this.title + ' by ' + this.author + ', ' + this.pages + ' pages, ' + this.readText;
+        let beenRead = readBoolean ? 'Read' : 'Not Read Yet';
+        return this.title + ' by ' + this.author + ', ' + this.pages + ' pages, ' + beenRead;
     }
+}
+
+Book.prototype.changeRead = function () {
+    this.read = !this.read;
+    this.readText = this.read ? 'You\'ve read this book!' : 'Not Read Yet';
 }
 
 const bookList = document.querySelector('.booklist');
@@ -26,25 +32,36 @@ function addBookToLibrary(e) {
     if (title && author && pages) {
     let newBook = new Book(title, author, pages, read);
     myLibrary.push(newBook);
-    let newCard = document.createElement('div'); //does this have to be a global variable to be removed later?
+    let newCard = document.createElement('div'); // Book Card
     newCard.classList.add('card')
-    let titleDiv = document.createElement('div');
+    let deleteCardBtn = document.createElement('button'); // Delete Button
+    deleteCardBtn.classList.add('deletecardbtn');
+    deleteCardBtn.textContent = 'X';
+    deleteCardBtn.addEventListener('click', deleteBookFromLibrary);
+    newCard.appendChild(deleteCardBtn);
+    let titleDiv = document.createElement('div'); // Title
     titleDiv.classList.add('title');
     titleDiv.textContent = newBook.title;
     newCard.appendChild(titleDiv);
-    let authorDiv = document.createElement('div');
+    let authorDiv = document.createElement('div'); //Author
     authorDiv.classList.add('author');
     authorDiv.textContent = newBook.author;
     newCard.appendChild(authorDiv);
-    let pagesDiv = document.createElement('div');
+    let pagesDiv = document.createElement('div');// Pages
     pagesDiv.classList.add('pages');
-    pagesDiv.textContent = newBook.pages;
+    pagesDiv.textContent = `${newBook.pages} pages`;
     newCard.appendChild(pagesDiv);
-    let readDiv = document.createElement('div');
+    let readDiv = document.createElement('div'); // Read Div
     readDiv.classList.add('read');
     readDiv.textContent = newBook.readText;
+    let readBtn = document.createElement('button'); // Read Button
+    readBtn.classList.add('readbtn');
+    readBtn.textContent = newBook.read ? 'Mark as Unread' : 'Mark as Read';
+    readBtn.addEventListener('click', changeReadStatus);
+    readDiv.appendChild(readBtn);
     newCard.appendChild(readDiv);
     bookList.appendChild(newCard);
+    assignData();
     document.querySelector('#title').value = '';
     document.querySelector('#author').value = '';
     document.querySelector('#pages').value = '';
@@ -69,4 +86,30 @@ function closeModal(e) {
     if (e.target === closeBtn || e.target === modal) {
     modal.style.display = 'none';
     }
+}
+
+function assignData() {
+    let bookCards = Array.from(document.querySelectorAll('.card'));
+    let deleteCardBtns = Array.from(document.querySelectorAll('.deletecardbtn'));
+    let readBtns = Array.from(document.querySelectorAll('.readbtn'))
+    for (let i = 0; i < myLibrary.length; i++) {
+        bookCards[i].setAttribute('data-myLibrary-index', [i]);
+        deleteCardBtns[i].setAttribute('data-myLibrary-index', [i])
+        readBtns[i].setAttribute('data-myLibrary-index', [i])
+    }
+}
+
+function deleteBookFromLibrary(e) {
+    let x = e.target.getAttribute('data-myLibrary-index'); // card and book object to be deleted
+    bookList.removeChild(bookList.children[x]);
+    myLibrary.splice(x, 1);
+    assignData();
+}
+
+function changeReadStatus(e) {
+    let x = e.target.getAttribute('data-myLibrary-index');
+    let parentDiv = e.target.parentElement;
+    myLibrary[x].changeRead();
+    parentDiv.firstChild.nodeValue = myLibrary[x].readText;
+    e.target.textContent = myLibrary[x].read ? 'Mark as Unread' : 'Mark as Read';
 }
